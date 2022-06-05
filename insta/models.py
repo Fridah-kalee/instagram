ecb8445ff2919b64fe9fa03238df798aa5e89464
Fridah-kalee <fridahkalee@gmail.com>
2022-06-05 07:00:39 +0300

    add forms.py file that allows us create new form fields

--- insta/models.py
+++ insta/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Profile(models.Model):
     name = models.CharField(max_length=30)
-    profile_pic = models.ImageField(upload_to='posts/', blank = 'true')
+    profile_pic = models.ImageField(upload_to='post/', blank = 'true')
     bio = models.TextField()
     user = models.OneToOneField(User, on_delete=models.CASCADE, default='', null=True)
 
