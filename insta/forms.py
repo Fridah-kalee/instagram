45fd1be02df5497aa3577aaa12e734513bc83fbb
Fridah-kalee <fridahkalee@gmail.com>
2022-06-05 09:08:21 +0300

    create signupform

--- insta/forms.py
+++ insta/forms.py
@@ -1,2 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
+
+class SignUpForm(forms.Form):
+    your_name=forms.CharField(label='First Name',max_length=30)
+    email=forms.EmailField(label='Email')
