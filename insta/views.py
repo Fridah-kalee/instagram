119cd17b8ae30e36587d843c5afabe24af5420c1
Fridah-kalee <fridahkalee@gmail.com>
2022-06-05 06:25:12 +0300

    creating home view function

--- insta/views.py
+++ insta/views.py
@@ -1,11 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import Profile,Post
+from django.contrib.auth.models import User
 
 # Create your views here.
 def home(request):
     posts = Post.all_posts()
     posts = []
     for post in posts:
-    
-     return render(request,'home.html')
+
+        pic = Profile.objects.filter(user=post.user.id).first()
+        if pic:
+            pic = pic.profile_pic.url
+        else:
+            pic =''
+        obj = dict(
+            image=post.image.url,
+            avatar=pic,
+            name=post.title,
+            author=post.user.name,
+            caption=post.caption
+            )
+
+        posts.append(obj)
+        return render(request,'home.html',{"posts:posts"})
