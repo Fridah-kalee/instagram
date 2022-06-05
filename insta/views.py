18dd910f11e538ab051d21bac218af519522981a
Fridah-kalee <fridahkalee@gmail.com>
2022-06-05 22:13:50 +0300

    sending welcome email functionality

--- insta/views.py
+++ insta/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import Profile,Post
-
+# from .forms import SignUpForm
 
 # Create your views here.
 def home(request):
